028692dcbc25901f06fea364cc1c7ede6eafade7
Nicholas Bor <nicholas.bor27@gmail.com>
2022-03-21 23:11:02 +1100

    regex for whitespace and while loops

--- Pytracker.py
+++ Pytracker.py
@@ -31,6 +31,8 @@ if traceback_switch == 1:
         
 
 
+steps = []
+visited_line = {}
     
 if trace_switch == 1:
 
@@ -45,22 +47,44 @@ if trace_switch == 1:
     exec_result = open(exec_result_file)
     exec_content = exec_result.readlines()
 
-    # set a dict for the while loop in execution
-    while_loop_dict = {}
-
     # parsing
     for line in exec_content:
         # use regular expression to match
         line_number = int(re.search("(\()(\d)(\):)(.*)", line.strip().replace(filename, "")).group(2))
         line_content = re.search("(\()(\d)(\):)(.*)", line.strip().replace(filename, "")).group(4)
-        print("line {} == {}".format(line_number, line_content))
+        # print("line {} == {}".format(line_number, line_content))
+
         if debug_mode : print(type(line_number), type(line_content))
 
-        if "while" in line_content:
-            if debug_mode: print("Warning: while loop detected.")
-            while_loop_dict[line_content] = [i for i,v in enumerate(exec_content) if v == line]
+        space = False
+        tab = False
+        space_sep = re.match(r"^[ ]{4}", line_content)
+        if not space_sep:
+            tab_sep = re.match(r"^[\t]{1}", line_content)
+        else:
+            res = re.sub(r"^[ ]{4}", "", line_content)
+            space = True
+            print(res)
 
-    print(while_loop_dict)
+        if tab_sep: 
+            res = re.sub(r"^[\t]{1}", "", line_content)
+            tab = True
+
+        is_while = re.search("^(while).*[:]$", res)
+        whitespace_match = None
+        if tab:
+            whitespace_match = re.search(r"^[\t]{1}", res)
+        elif space:
+            whitespace_match = re.search(r"^[ ]{4}", res)
+
+        if is_while and line_number not in visited_line:
+            if debug_mode: print("Warning: while loop detected.")
+            dict_cont = line_content.strip()
+        
+        visited_line[line_number] = line_content
 
 
     exec_result.close()
+
+# print(visited_line)
+# print(steps)
