c9621095fffd0ead91cdb6b33cc878449ef94bf1
yst990102 <z5192519@ad.unsw.edu.au>
2022-06-01 20:28:12 +1000

    06-01 add parse_classes.py

---
+++ backend/parse_classes.py
@@ -0,0 +1,16 @@
+class Program():
+    
+    def __init__(self) -> None:
+        pass
+        
+class While_Loop():
+    def __init__(self) -> None:
+        pass
+        
+class Assignment():
+    def __init__(self) -> None:
+        pass
+
+class Functions():
+    def __init__(self) -> None:
+        pass
