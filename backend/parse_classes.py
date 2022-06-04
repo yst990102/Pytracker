ba8e72358c4f23e850d38548d32be2fd9cf3d430
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-06-04 17:59:15 +1000

    06-04 inner statement linklist set success

--- backend/parse_classes.py
+++ backend/parse_classes.py
@@ -1,25 +1,25 @@
-
-swithc_statement_print = False
-
 class Program():
     statements = []
     def __init__(self):
         return
     
     def add_statement(self, statement):
+        # first statement in program, no need to set_previous or set_next
         if len(self.statements) < 1:
             self.statements.append(statement)
-            print("no need to set previous/next")
             return
+        # everytime append a new statement
+        # set_previous & set_next for new statemenet
+        # set_next for the previous statement in self.statements
         else:
+            # new_statement set_previous and set_next
             statement.set_previous(self.statements[-1])
             statement.set_next(None)
             
+            # previous statement in self.statements set_next
             self.statements[-1].set_next(statement)
             
             self.statements.append(statement)
-            
-            print("previous/next set")
     
     def print(self):
         for statement in self.statements:
@@ -30,12 +30,22 @@ class Statement():
         self.steps = []
         self.previous = None
         self.next     = None
-        
+    
+    # set the previous statement
     def set_previous(self, previous):
         self.previous = previous
-        
+    
+    # set the next statement
     def set_next(self, next):
         self.next = next
+    
+    # get the previous statement
+    def get_previous(self):
+        return self.previous
+    
+    # get the next statement
+    def get_next(self):
+        return self.next
 
 class While_Loop(Statement):
     general_steps = []
@@ -43,6 +53,7 @@ class While_Loop(Statement):
         super().__init__()
         print(f"---- create While_Loop {steps}")
         
+        # add statements for self.steps
         self.general_steps = steps
         for step in steps:
             if isinstance(step, int):
@@ -50,7 +61,13 @@ class While_Loop(Statement):
             elif isinstance(step, list):
                 new_statement = While_Loop(step)
             self.steps.append(new_statement)
-            
+
+        # set the inner bi-linklist in self.steps
+        for i in range(len(self.steps)):
+            if i != 0:
+                self.steps[i].set_previous(self.steps[i-1])
+            if i != len(self.steps) - 1:
+                self.steps[i].set_next(self.steps[i+1])
         return
 
     def print(self):
