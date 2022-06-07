01805b30c72a4a0e3b7f7e866ec5dd5f5f7e56ea
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-06-07 13:23:36 +1000

    06-07 update

--- backend/helper_functions.py
+++ backend/helper_functions.py
@@ -4,20 +4,20 @@ import re
 
 # helper functions
 def export_test_case_to_file(output_file:str, test_case:str) -> None:
-    with open(output_file, "w") as f_write:
-        f_write.write(test_case)
-    f_write.close()
-    return
+	with open(output_file, "w") as f_write:
+		f_write.write(test_case)
+	f_write.close()
+	return
 
 def delete_file(file_name:str) -> None:
-    os.remove(file_name)
-    return
+	os.remove(file_name)
+	return
 
 def display_file(file_name:str) -> None:
-    with open(file_name, 'r') as f_read:
-        content = f_read.read()
-    print(content)
-    f_read.close()
+	with open(file_name, 'r') as f_read:
+		content = f_read.read()
+	print(content)
+	f_read.close()
 
 def isBracket_match(s:str) -> bool:
 	finding = []
@@ -45,7 +45,7 @@ def del_line_in_file(filename, content):
 	f.close()
 	f_w.close()
 
-def clean_txt_file(filename):
+def clean_content_in_file(filename):
 	try:
 		with open(filename, "r+") as f:
 			f.seek(0)
@@ -57,14 +57,26 @@ def clean_txt_file(filename):
 		open(filename, 'w').close()
 		return
 
-def NameEqMain_check(file_name):
-	with open(file_name, 'r') as f:
-		f_content = f.read()
-		if re.search(f_content, "if[ ]*__name__[ ]*==[ ]*\"__main__\"[ ]*:"):
-			# user input contains __name__ == __main__ structure
+def create_test_file(user_code_input_file, output_test_file):
+		def_main_str = "def main():\n"
+		if_NameEqMain_str = "\n\nif __name__ == \"__main__\":\n\tmain()"
+		with open(user_code_input_file, 'r') as user_code:
+			if NameEqMain_check(user_code_input_file):
+				testing_user_code = user_code.read()
+			else:
+				testing_user_code = def_main_str + "".join(["\t" + i for i in user_code.readlines()]) + if_NameEqMain_str
+		user_code.close()
+		
+		with open(output_test_file, 'w') as user_code_output:
+			user_code_output.write(testing_user_code)
+		user_code_output.close()
+
+def NameEqMain_check(filename):
+	with open(filename, 'r') as f:
+		file_content = f.read()
+		if re.search("if[ ]*__name__[ ]*==[ ]*\"__main__\"[ ]*:", file_content):
 			return True
-		else:
-			return False
+	return False
 			
 def add_main_to_file(file_name):
 	return
