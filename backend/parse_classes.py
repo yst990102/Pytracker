377b172b2222a3ca3049f00050ab650cff3c116c
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-06-07 14:55:46 +1000

    06-07 add while_loops in program()

--- backend/parse_classes.py
+++ backend/parse_classes.py
@@ -7,9 +7,10 @@ Print_Backward = 1
 yst_personal_debug = True
 
 class Statement():
-    def __init__(self) -> None:
+    def __init__(self, program) -> None:
         self.previous = None
         self.next     = None
+        self.program  = program
     
     # set the previous statement
     def set_previous(self, previous) -> None:
@@ -44,12 +45,17 @@ class Statement():
             return pointer.get_first_inner_step()
 
 class While_Loop(Statement):
-    def __init__(self, steps:List[List]) -> None:
-        super().__init__()
+    def __init__(self, steps:List[List], program) -> None:
+        super().__init__(program)
         
         if yst_personal_debug: print(f"---- create {self.__class__.__name__} {steps}")
         self.general_steps = steps
         self.steps = []
+        self.while_line_no = self.general_steps[0]
+        
+        # add while_loop to program.while_loops attribute
+        assert(isinstance(self.program, Program))
+        self.program.add_while_loop(self)
             
     def inner_bi_linklist_set(self) -> None:
         # set the inner bi-linklist in self.steps
@@ -84,8 +90,8 @@ class While_Loop(Statement):
         if yst_personal_debug: print(self.general_steps, end=" ")
 
 class Basic_While_Loop(While_Loop):
-    def __init__(self, steps:List[List]) -> None:
-        super().__init__(steps)
+    def __init__(self, steps:List[List], program) -> None:
+        super().__init__(steps, program)
         
         self.add_sub_statements(steps)
         self.inner_bi_linklist_set()
@@ -93,12 +99,12 @@ class Basic_While_Loop(While_Loop):
     def add_sub_statements(self, steps:List[List]) -> None:
         # add statements for self.steps
         for step in steps:
-            new_statement = Assignment(step)
+            new_statement = Assignment(step, self.program)
             self.steps.append(new_statement)
 
 class Nested_While_Loop(While_Loop):    
-    def __init__(self, steps:List[List]) -> None:
-        super().__init__(steps)
+    def __init__(self, steps:List[List], program) -> None:
+        super().__init__(steps, program)
         self.sub_while_loops = []
         
         self.add_sub_statements(steps)
@@ -108,16 +114,16 @@ class Nested_While_Loop(While_Loop):
         # add statements for self.steps
         for step in steps:
             if isinstance(step, int):
-                new_statement = Assignment(step)
+                new_statement = Assignment(step, self.program)
             elif isinstance(step, list):
                 if all(isinstance(i, int) for i in step):
-                    new_statement = Basic_While_Loop(step)
+                    new_statement = Basic_While_Loop(step, self.program)
                 else:
-                    new_statement = Nested_While_Loop(step)
+                    new_statement = Nested_While_Loop(step, self.program)
                 self.sub_while_loops.append(new_statement)
-                if yst_personal_debug: print(f"before append {self.sub_while_loops}")
-                if yst_personal_debug: print(f"append {new_statement.__class__.__name__} {hex(id(new_statement))} to {self.__class__.__name__} {hex(id(self))}")
-                if yst_personal_debug: print(f"after append {self.sub_while_loops}")
+                
+                # 06-07 print out test for appending loop to loop
+                # if yst_personal_debug: print(f"append {new_statement.__class__.__name__} {hex(id(new_statement))} to {self.__class__.__name__} {hex(id(self))}")
             self.steps.append(new_statement)
     
     def print_info(self) -> None:
@@ -125,8 +131,8 @@ class Nested_While_Loop(While_Loop):
         if yst_personal_debug: print(f"{self.__class__.__name__} {hex(id(self))} sub_while_loop = {self.sub_while_loops}")
     
 class Assignment(Statement):
-    def __init__(self, line_no:int) -> None:
-        super().__init__()
+    def __init__(self, line_no:int, program) -> None:
+        super().__init__(program)
         if yst_personal_debug: print(f"---- create {self.__class__.__name__} {line_no}")
         self.line_no = line_no
 
@@ -141,6 +147,28 @@ class Assignment(Statement):
 class Program():
     def __init__(self) -> None:
         self.statements = []
+        self.while_loops = []
+    
+    def add_while_loop(self, new_while_loop:While_Loop):
+        new_while_line_no = new_while_loop.while_line_no
+        
+        refered_found = False
+        for while_loop_info in self.while_loops:
+            cur_while_line_no = while_loop_info.get("while_line_no")
+            cur_while_iterations = while_loop_info.get("iterations")
+            if new_while_line_no == cur_while_line_no:
+                cur_while_iterations.append(new_while_loop.general_steps)
+                refered_found = True
+                break
+        if not refered_found:
+            self.while_loops.append({"while_line_no": new_while_loop.while_line_no, "iterations": [new_while_loop.general_steps]})
+        
+        # 06-07 test for print out self.while_loops
+        # self.print_while_loops_inlayer()
+        
+    def print_while_loops_inlayer(self):
+        for while_loop_info in self.while_loops:
+            if yst_personal_debug: print(f"while_loop_info == {while_loop_info}")
     
     def get_first_process(self) -> Statement:
         pointer = self.statements[0]
