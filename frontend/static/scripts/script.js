c31c61d0485dd303129ba3eea4ccff5c09d379c7
Nicholas Bor <nicholas.bor27@gmail.com>
2022-06-12 23:07:35 +1000

    given code is parsed into html table format

--- frontend/static/scripts/script.js
+++ frontend/static/scripts/script.js
@@ -1,4 +1,6 @@
-const analyseCodeBtn = document.querySelector('.editor__btn');
+const analyseCodeBtn = document.querySelector('.editor_btn');
+const editordiv = document.getElementById("inputeditor")
+const outputdiv = document.getElementById("output")
 
 let editor = ace.edit("editor");
 
@@ -14,7 +16,32 @@ let editorlib = {
 }
 
 $("#codeSubmit").click(() => {
+    if (editordiv.style.display !== "none") {
+        editordiv.style.display = "none";
+    }
+
     const usercode = editor.getValue();
+    var lines = usercode.split('\n');
+    parselist = []
+    for (var i = 1; i <= lines.length; i++) {
+        parselist.push({
+            num: i,
+            content: lines[i - 1]
+        })
+    }
+    console.log(parselist)
+
+    var table = $("#code_output")
+    parselist.forEach((dt) => {
+        line_num = dt["num"]
+        line_content = dt["content"]
+        console.log(line_num)
+        console.log(line_content)
+        markup = '<tr><td class="line_num" style="white-space: pre;">' + line_num + '</td><td class="line_content" style="white-space: pre;">' + line_content + '</td></tr>';
+        table.append(markup);
+
+    });
+
     $.ajax({
         type: 'POST',
         url: "/",
