105f32c3bd26a02a77a4cbab7285bcae1ed6f60e
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-08-16 14:44:35 +1000

    staged if_else_tests

--- backend/tests/if_else_test.py
+++ backend/tests/if_else_test.py
@@ -4,13 +4,8 @@ import pathlib
 current_absolute_path = str(pathlib.Path(__file__).parent.resolve())
 
 class Test_IF_Statement():
-
-	def test_if(self):
-		usercode = """a = 10
-b = 20
-if a < b:
-	print("HELLO")
-"""
+	def test_simplest(self):
+		usercode = """a = 10\nb = 20\nif a < b:\n\tprint("HELLO")"""
 		with open(current_absolute_path + "/../" + "UserCode.py", 'w') as f_w:
 			f_w.write(usercode)
 		f_w.close()
@@ -22,15 +17,20 @@ if a < b:
 
 		assert (listoflist == [1, 2, 4])
 		assert (step_json == {"d": 5, "list": [{"type": "step", "start": 0, "end": 1}, {"type": "step", "start": 1, "end": 2}, {"type": "step", "start": 2, "end": 4}]})
+	
+	def test_nested(self):
+		pass
+	
+	def test_with_while(self):
+		pass
 
-	def test_ifelse(self):
-		usercode = """a = 10
-b = 20
-if b < a:
-	print("Hello")
-else:
-	print("BYE")
-"""
+	def test_true_or_false(self):
+		pass
+
+
+class Test_IF_Else_Statement():
+	def test_simplest(self):
+		usercode = """a = 10\nb = 20\nif b < a:\n\tprint("Hello")\nelse:\n\tprint("BYE")"""
 		with open(current_absolute_path + "/../" + "UserCode.py", 'w') as f_w:
 			f_w.write(usercode)
 		f_w.close()
@@ -42,4 +42,26 @@ else:
 
 		assert (listoflist == [1, 2, 6])
 		assert (step_json == {"d": 5, "list": [{"type": "step", "start": 0, "end": 1}, {'type': 'step', 'start': 1, 'end': 2}, {'type': 'step', 'start': 2, 'end': 6}]})
+	
+	def test_nested(self):
+		pass
 
+	def test_with_while(self):
+		pass
+
+	def test_true_or_false(self):
+		pass
+
+
+class Test_IF_Elif_Statement():
+	def test_simplest(self):
+		pass
+	
+	def test_nested(self):
+		pass
+
+	def test_with_while(self):
+		pass
+
+	def test_true_or_false(self):
+		pass
