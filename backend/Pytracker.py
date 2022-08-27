609c1b4af935b9fbe7af2e677a12488a2df17f11
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-08-28 01:41:58 +1000

    add signals and relevant parameter in backend_main

--- backend/Pytracker.py
+++ backend/Pytracker.py
@@ -16,8 +16,9 @@ current_absolute_path = str(pathlib.Path(__file__).parent.resolve())
 # DEBUG switches
 DEBUG_parse_strListOfList_into_ListOfList = False
 
-# TEST_SIGNAL
-TEST_TIME_COST = 0
+# SIGNAL
+SIG_TIME_COST = 0
+SIG_FILE_IO_OFF = 1
 
 def trace_execution_tracking(result_file):
 	# steps -- all the execution steps
@@ -183,9 +184,9 @@ def parse_convert_TupleOfIntTuple_into_Program(TupleOfIntAndTuple, tab_dict: dic
 	return parse_classes.Program(TupleOfIntAndTuple, tab_dict, grid_indent)
 
 
-def backend_main(usercode=None, test_signal=None):
+def backend_main(*test_signals, usercode=None):
 	# region [Stage 1.1](reformat user's code)
-	if test_signal == TEST_TIME_COST: reformat_start_time = time.time()
+	if SIG_TIME_COST in test_signals: reformat_start_time = time.time()
 	global reformatted_code
 	if usercode == None:
 		usercode = open(current_absolute_path + "/" + "UserCode.py", 'r').read()
@@ -193,8 +194,8 @@ def backend_main(usercode=None, test_signal=None):
 		reformatted_code, encoding, changed = FormatFile(filename=f"{current_absolute_path}/UserCode.py", style_config=f"{current_absolute_path}/.style.yapf")
 	else:
 		reformatted_code, changed = FormatCode(unformatted_source=usercode, style_config=f"{current_absolute_path}/.style.yapf")
-	if test_signal == TEST_TIME_COST: reformat_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 1.1](reformat user's code) \t\t\t: {reformat_end_time - reformat_start_time} seconds")
+	if SIG_TIME_COST in test_signals: reformat_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 1.1](reformat user's code) \t\t\t: {reformat_end_time - reformat_start_time} seconds")
 	# endregion
 	
 	
@@ -203,15 +204,16 @@ def backend_main(usercode=None, test_signal=None):
 	
 	
 	# region [Stage 1.2](pre_clean the wrote_output file)
-	if test_signal == TEST_TIME_COST: file_clean_start_time = time.time()
+	if SIG_TIME_COST in test_signals: file_clean_start_time = time.time()
 	
-	hf.clean_content_in_file(current_absolute_path + "/" + "listoflist")
-	hf.clean_content_in_file(current_absolute_path + "/" + "TupleOfIntAndTuple")
-	hf.clean_content_in_file(current_absolute_path + "/" + "step_json.json")
-	hf.clean_content_in_file(current_absolute_path + "/" + "Pytracker_output")
+	if SIG_FILE_IO_OFF not in test_signals:
+		hf.clean_content_in_file(current_absolute_path + "/" + "listoflist")
+		hf.clean_content_in_file(current_absolute_path + "/" + "TupleOfIntAndTuple")
+		hf.clean_content_in_file(current_absolute_path + "/" + "step_json.json")
+		hf.clean_content_in_file(current_absolute_path + "/" + "Pytracker_output")
 	
-	if test_signal == TEST_TIME_COST: file_clean_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 1.2](pre_clean the wrote_output file) \t\t: {file_clean_end_time - file_clean_start_time} seconds")
+	if SIG_TIME_COST in test_signals: file_clean_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 1.2](pre_clean the wrote_output file) \t\t: {file_clean_end_time - file_clean_start_time} seconds")
 	# endregion
 
 
@@ -220,13 +222,13 @@ def backend_main(usercode=None, test_signal=None):
 
 
 	# region [Stage 1.3](create tracer and run)
-	if test_signal == TEST_TIME_COST: tracer_start_time = time.time()
+	if SIG_TIME_COST in test_signals: tracer_start_time = time.time()
 	
 	tracer = my_trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix], trace=1, count=1, outfile=current_absolute_path + "/" + "Pytracker_output")
 	tracer.run(reformatted_code)
 	
-	if test_signal == TEST_TIME_COST: tracer_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 1.3](create tracer and run) \t\t\t: {tracer_end_time - tracer_start_time} seconds")
+	if SIG_TIME_COST in test_signals: tracer_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 1.3](create tracer and run) \t\t\t: {tracer_end_time - tracer_start_time} seconds")
 	# endregion
 	
 	
@@ -235,7 +237,7 @@ def backend_main(usercode=None, test_signal=None):
 	
 	
 	# region [Stage 2.1](generate list of list via trace_output)
-	if test_signal == TEST_TIME_COST: listoflist_start_time = time.time()
+	if SIG_TIME_COST in test_signals: listoflist_start_time = time.time()
 	
 	# trace the whole execution, return a ListOfList
 	global listoflist
@@ -245,13 +247,14 @@ def backend_main(usercode=None, test_signal=None):
 	listoflist = hf.remove_singlelist_from_listoflist(listoflist)
 	listoflist = hf.remove_if_else_lines_from_listoflist(if_else_lines, listoflist)
 
-	# write listoflist into "listoflist"
-	with open(current_absolute_path + "/" + "listoflist", 'w') as listoflist_out:
-		listoflist_out.write(str(listoflist))
-	listoflist_out.close()
+	if SIG_FILE_IO_OFF not in test_signals:
+		# write listoflist into "listoflist"
+		with open(current_absolute_path + "/" + "listoflist", 'w') as listoflist_out:
+			listoflist_out.write(str(listoflist))
+		listoflist_out.close()
 	
-	if test_signal == TEST_TIME_COST: listoflist_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 2.1](generate list of list via trace_output) \t: {listoflist_end_time - listoflist_start_time} seconds")
+	if SIG_TIME_COST in test_signals: listoflist_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 2.1](generate list of list via trace_output) \t: {listoflist_end_time - listoflist_start_time} seconds")
 	# endregion
 
 
@@ -259,14 +262,16 @@ def backend_main(usercode=None, test_signal=None):
 
 
 	# region [Stage 2.2](convert list of list to program)
-	if test_signal == TEST_TIME_COST: program_generate_start_time = time.time()
+	if SIG_TIME_COST in test_signals: program_generate_start_time = time.time()
 	
 	# convert ListOfList into TupleOfIntAndTuple
 	TupleOfIntAndTuple = hf.ListOfList_to_ListOfIntAndTuple(listoflist)
-	# write listoflist into "listoflist"
-	with open(current_absolute_path + "/" + "TupleOfIntAndTuple", 'w') as TupleOfIntAndTuple_out:
-		TupleOfIntAndTuple_out.write(str(TupleOfIntAndTuple))
-	TupleOfIntAndTuple_out.close()
+	
+	if SIG_FILE_IO_OFF not in test_signals:
+		# write listoflist into "listoflist"
+		with open(current_absolute_path + "/" + "TupleOfIntAndTuple", 'w') as TupleOfIntAndTuple_out:
+			TupleOfIntAndTuple_out.write(str(TupleOfIntAndTuple))
+		TupleOfIntAndTuple_out.close()
 
 	grid_indent = hf.tabdict_to_gridindent(tab_dict, while_lines)
 	# then convert into Program
@@ -278,8 +283,8 @@ def backend_main(usercode=None, test_signal=None):
 	# program.print_linklist(parse_classes.Print_Backward)
 	# program.print_while_loops_inlayer()
 	
-	if test_signal == TEST_TIME_COST: program_generate_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 2.2](convert list of list to program) \t\t: {program_generate_end_time - program_generate_start_time} seconds")
+	if SIG_TIME_COST in test_signals: program_generate_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 2.2](convert list of list to program) \t\t: {program_generate_end_time - program_generate_start_time} seconds")
 	# endregion
 
 
@@ -287,23 +292,24 @@ def backend_main(usercode=None, test_signal=None):
 
 
 	# region [Stage 3.1](get step_json via program)
-	if test_signal == TEST_TIME_COST: json_generate_start_time = time.time()
+	if SIG_TIME_COST in test_signals: json_generate_start_time = time.time()
 	
 	global step_json
 	step_json = hf.get_step_json(program)
 
-	# write step_json into "step_json"
-	with open(current_absolute_path + "/" + "step_json.json", "w") as step_json_file_write:
-		json.dump(step_json, step_json_file_write)
-	step_json_file_write.close()
+	if SIG_FILE_IO_OFF not in test_signals:
+		# write step_json into "step_json"
+		with open(current_absolute_path + "/" + "step_json.json", "w") as step_json_file_write:
+			json.dump(step_json, step_json_file_write)
+		step_json_file_write.close()
 	
-	if test_signal == TEST_TIME_COST: json_generate_end_time = time.time()
-	if test_signal == TEST_TIME_COST: print(f"[Stage 3.1](get step_json via program) \t\t\t: {json_generate_end_time - json_generate_start_time} seconds")
+	if SIG_TIME_COST in test_signals: json_generate_end_time = time.time()
+	if SIG_TIME_COST in test_signals: print(f"[Stage 3.1](get step_json via program) \t\t\t: {json_generate_end_time - json_generate_start_time} seconds")
 	# endregion
 	
 	
 	
-	if test_signal == TEST_TIME_COST: print(f"--------------- All Stages --------------- \t\t: {json_generate_end_time - reformat_start_time} seconds")
+	if SIG_TIME_COST in test_signals: print(f"--------------- All Stages --------------- \t\t: {json_generate_end_time - reformat_start_time} seconds")
 	return listoflist, TupleOfIntAndTuple, program, step_json
 
 if __name__ == "__main__":
