dc8b7cadab85d30301938875e849102598b59eb6
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-09-12 16:05:04 +1000

    update comment

--- backend/tests/for_test.py
+++ backend/tests/for_test.py
@@ -94,22 +94,22 @@ for i in range(4):
 print(even_sum)
 print(odd_sum)
 """
-		# 应该获取与如下代码类似结果
-		# even_sum = 0
-		# odd_sum = 0
-		# i = 0
-		# j = 4
-		# while i < 4:
-		# 	while j < 6:
-		# 		if (i + j) % 2 == 0:
-		# 			even_sum += (i + j)
-		# 		else:
-		# 			odd_sum += (i + j)
-		# 		j += 1
-		# 	i += 1
+# 应该获取与如下代码类似结果
+# even_sum = 0
+# odd_sum = 0
+# i = 0
+# j = 4
+# while i < 4:
+# 	while j < 6:
+# 		if (i + j) % 2 == 0:
+# 			even_sum += (i + j)
+# 		else:
+# 			odd_sum += (i + j)
+# 		j += 1
+# 	i += 1
 
-		# print(even_sum)
-		# print(odd_sum)
+# print(even_sum)
+# print(odd_sum)
 
 		with open(backend_absolute_path + "/" + "UserCode.py", 'w') as f_w:
 			f_w.write(usercode)
