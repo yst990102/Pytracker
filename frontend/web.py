93830f419a8425453c8b170011093ea4be784acd
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-09-20 10:20:52 +1000

    1. add SIG_FILE_IO to web.py 2. add filelock to frontend_usercode_file

--- frontend/web.py
+++ frontend/web.py
@@ -21,6 +21,11 @@ import feedback_email
 
 app = Flask(__name__, template_folder=frontend_absolute_path + '/templates/')
 
+from filelock import Timeout, FileLock
+frontend_usercode_file_lock = FileLock("frontend_usercode_file.lock")
+
+
+
 def input(__prompt: object = ...) -> str:
 	userinput = next(userinput_iter)
 	return userinput['userinput']
@@ -41,29 +46,26 @@ def traceback_checking():
         usercode = request.json
         reformatted_code, changed = FormatCode(unformatted_source=usercode, style_config=f"{backend_absolute_path}/.style.yapf")
         
-        with open(backend_absolute_path + '/UserCode.py', 'w') as f_write:
-            f_write.write(reformatted_code)
-        f_write.close()
-        
-        
-        # tracebacke v1.0
-        # traceback_result = subprocess.Popen(['python '+ backend_absolute_path + '/UserCode.py'], stderr=subprocess.PIPE, shell=True).stderr.read().decode("utf-8")
-        
-        input_cmd = []
-        for userinput in userinput_list:
-            input_cmd.append(userinput['userinput'])
-        
-        # tracebacke v1.1
-        traceback_subprocess = subprocess.Popen(['python '+ backend_absolute_path + '/UserCode.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        
-        output, err = traceback_subprocess.communicate(input='\n'.join(input_cmd).encode())
-        output_msg = output.decode('utf-8')
-        err_msg = err.decode('utf-8')
-        print(f"err_msg = {err_msg}")
-        if err_msg:
-            return {'error': True, 'error_msg': err_msg}
-        else:
-            return {'error': False}
+        with frontend_usercode_file_lock:
+            with open(backend_absolute_path + '/UserCode.py', 'w') as f_write:
+                f_write.write(reformatted_code)
+            f_write.close()
+
+            input_cmd = []
+            for userinput in userinput_list:
+                input_cmd.append(userinput['userinput'])
+            
+            # tracebacke v1.1
+            traceback_subprocess = subprocess.Popen(['python '+ backend_absolute_path + '/UserCode.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            
+            output, err = traceback_subprocess.communicate(input='\n'.join(input_cmd).encode())
+            output_msg = output.decode('utf-8')
+            err_msg = err.decode('utf-8')
+            if err_msg:
+                return {'error': True, 'error_msg': err_msg}
+            else:
+                return {'error': False}
+
     return render_template('index.html')
 
 
@@ -72,7 +74,7 @@ def home_page():
     if request.method == "POST":
 
         # run backend_main
-        Pytracker.backend_main(usercode=request.json, userinput_iter=userinput_iter)
+        Pytracker.backend_main(Pytracker.SIG_FILE_IO_OFF,usercode=request.json, userinput_iter=userinput_iter)
 
         # grab the result of listoflist and step_json from global variables
         listoflist = Pytracker.listoflist
