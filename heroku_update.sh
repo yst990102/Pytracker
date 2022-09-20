2338c55aa5de09248cc4c2ca46582a32d445cba7
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-09-20 12:15:54 +1000

    add filelock to requirement.txt

--- heroku_update.sh
+++ heroku_update.sh
@@ -1,5 +1,5 @@
 heroku login
 heroku container:login
-heroku container:push web -a pytracker-input
-heroku container:release web -a pytracker-input
-heroku open -a pytracker-input
+heroku container:push web -a pytracker-dev
+heroku container:release web -a pytracker-dev
+heroku open -a pytracker-dev
