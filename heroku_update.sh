d4ba833b702b4f78090674289d834f5fb73909f5
Shi Tong Yuan <shitong.yuan@student.unsw.edu.au>
2022-11-07 15:03:31 +1100

    change app name to pytracker

--- heroku_update.sh
+++ heroku_update.sh
@@ -1,5 +1,5 @@
 heroku login
 heroku container:login
-heroku container:push web -a pytracker-dev
-heroku container:release web -a pytracker-dev
-heroku open -a pytracker-dev
+heroku container:push web -a pytracker
+heroku container:release web -a pytracker
+heroku open -a pytracker
